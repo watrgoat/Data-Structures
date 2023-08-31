81b81951bc94ab66ec05bd577b02e5a71e412b88
watrgoat <oliveradams@iceeducate.org>
2023-08-31 14:21:17 -0400

    initial commit

---
+++ HW/HW-1-Text-Justification/justify.cpp
@@ -0,0 +1,9 @@
+/* This program reads words from an input file and
+packs the words one at a time onto each line. */
+
+# include <iostream>
+# include <fstream>
+
+int main(int argc, char* argv[]) {
+	for (i)
+}
