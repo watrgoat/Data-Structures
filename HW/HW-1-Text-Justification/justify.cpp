ba72dabfd51875fd203f1d3fe2e23e654b074cb1
watrgoat <oliveradams@iceeducate.org>
2023-08-31 14:59:10 -0400

    initial commit

--- HW/HW-1-Text-Justification/justify.cpp
+++ HW/HW-1-Text-Justification/justify.cpp
@@ -1,9 +1,47 @@
-/* This program reads words from an input file and
-packs the words one at a time onto each line. */
+/*
+example.txt:
+
+Here is an example of text justification.
+
+And here’s the output produced for that input file for 
+the three different justification modes, for the specified
+text width of 16:
+
+flush left:
+--------------------
+| Here is an       |
+| example of text  |
+| justification.   |
+--------------------
+
+flush right:
+--------------------
+|       Here is an |
+|  example of text |
+|   justification. |
+--------------------
+
+full justify:
+--------------------
+| Here    is    an |
+| example  of text |
+| justification.   |
+--------------------*/
 
 # include <iostream>
 # include <fstream>
 
 int main(int argc, char* argv[]) {
-	for (i)
+	if (argc<5) {
+		std::cerr << "ERROR: Not enough input arguments." << std::endl;
+		return 0;
+	}
+	char inFileName = argv[1];
+	char outFileName = argv[2];
+	int textWidth = atoi(argv[3]);
+
+	// argv[1]: input file name
+	// argv[2]: output file name
+	// argv[3]: text width
+	// argv[4]: 
 }
