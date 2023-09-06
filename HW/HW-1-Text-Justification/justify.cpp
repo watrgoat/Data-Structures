413e97d7536522135ec8c03dd30e7afd8a63a0aa
watrgoat <oliveradams@iceeducate.org>
2023-09-06 01:44:41 -0400

    worked on hw

--- HW/HW-1-Text-Justification/justify.cpp
+++ HW/HW-1-Text-Justification/justify.cpp
@@ -15,17 +15,11 @@ using namespace std;
 // function to read text and split up words
 // possible change: bool func to output success
 bool GetText(ifstream &file, vector<string> &textArr) {
-	if (!file.good()) {
-	    cerr << "Can't open " << fileName << endl;
-	    exit(1);
-	}
-
 	string line;
 
 	while (file >> line) {
 	    string word;
 	    stringstream ss(line);
-
 	    while (ss >> word) {
 	        textArr.push_back(word);
 	    }
@@ -37,27 +31,32 @@ bool GetText(ifstream &file, vector<string> &textArr) {
 
 
 // function for flush left pass text by const reference
-bool FlushLeft(ofstream &oFile, const vector<string> &lineVec) {
+bool FlushLeft(ofstream &file, const vector<string> &lineVec, const unsigned int &width) {
 	string line;
 	for (unsigned int i; i < lineVec.size(); i++) {
-		line += lineVec
+		line += lineVec[i] + ' ';
 	}
-	// loop over words serperate by a space
-	// add the rest of spaces at the end
+	// remove extra space at the end
+	line.pop_back();
+
+	// adds the rest of spaces at the end
+	string extraSpaces(width-line.length(), ' ');
+	cout << line << extraSpaces << endl;
+
 	return true;
 }
 
 
 // function for flush right
-bool FlushRight(ofstream &oFile, const vector<string> &lineVec) {
+bool FlushRight(ofstream &file, const vector<string> &lineVec, const unsigned int &width) {
 	
 	return true;
 }
 
 
 // function for full justify
-bool FullJustify(ofstream &oFile, const vector<string> &lineVec) {
-
+bool FullJustify(ofstream &file, const vector<string> &lineVec, const unsigned int &width) {
+	
 	return true;
 }
 // divide the extra space and place it in between words, use floor div and modulo to contain remainder
@@ -70,10 +69,11 @@ bool FullJustify(ofstream &oFile, const vector<string> &lineVec) {
 // start and endline values are updated to max line length.
 // if single word is > than maxWidth catch it, set it to line, chop it like a tree, at max width-1 add a hyphen until lineLength < maxWidth
 // once lineLength is too full add lines to file, repeat until reaching end of 
-bool LineSplitter(ofstream &outFile, const unsigned int &maxWidth, const vector<string> &textArr, const string &textType) {
+bool LineSplitter(ofstream &oFile, const unsigned int &maxWidth, const vector<string> &textArr, const string &textType) {
 	int lineLength = 0;
 	vector<string> line;
 	string longLine;
+	cout << "TEST" << endl;
 	
 	for (unsigned int i; i<textArr.size(); i++) {
 		if (textArr[i].length() > maxWidth) {
@@ -81,6 +81,7 @@ bool LineSplitter(ofstream &outFile, const unsigned int &maxWidth, const vector<
 
 			if  (lineLength > 0) {
 				// CALL FLUSH HERE
+				FlushLeft(oFile, line, maxWidth);
 				line.clear();
 				lineLength = 0;
 			}
@@ -93,21 +94,24 @@ bool LineSplitter(ofstream &outFile, const unsigned int &maxWidth, const vector<
 			}
 
 			if (longLine.length() > 0) {
-				line.push_back(longLine)
+				line.push_back(longLine);
 				lineLength = longLine.length();
 				longLine.clear();
 			}
 		} else {
 			// word not too long
 
-			if (textArr[i].length() + 1 + lineLength > maxWidth) {
+			if (textArr[i].length() + (line.size()-1) + lineLength > maxWidth) {
 				// CALL FLUSH HERE
+				// check if line is last for full just and call left just instead
+				FlushLeft(oFile, line, maxWidth);
+
 				line.clear();
 				lineLength = 0;
 			} else {
 				// line not too long
-				line.push_back(textArr[i])
-				lineLength += textArr[i].length()
+				line.push_back(textArr[i]);
+				lineLength += textArr[i].length();
 			}
 		}
 	}
@@ -126,20 +130,28 @@ int main(int argc, char* argv[]) {
 	unsigned int textWidth = atoi(argv[3]); // text width
 	string flushType = argv[4]; // flush_left, flush_right, or full_justify
 
-	if (flushType!="flush_left" || flushType!="flush_right" || flushType!="full_justify") {
+/*	if (flushType!="flush_left" || flushType!="flush_right" || flushType!="full_justify") {
 		cerr << "ERROR: " << flushType << " is not an option." << endl;
 		exit(1);
-	}
-
+	}*/
 
 	vector<string> text;
-
-	// gets text from input file
-	GetText(inFileName, text);
-
-	bool success = false;
-
+	string dashedLine(textWidth+4, '-');
 	
 
+	// gets text from input file
+	GetText(inFile, text);
+
+	for (int i; text.size(); i++) {
+		cout << text[i] << endl;
+	}
+
+	cout << dashedLine << endl;
+
+	LineSplitter(outFile, textWidth, text, flushType);
+
+	cout << dashedLine << endl;
+
+	outFile.close();
 	return 0;
 }
