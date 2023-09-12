f02e84d2647fd1cee2af1ddb38a6ac0f3d3f66ee
watrgoat <oliveradams@iceeducate.org>
2023-09-11 23:11:57 -0400

    work hw2

--- HW/HW-2-Course-Scheduling/scheduling.cpp
+++ HW/HW-2-Course-Scheduling/scheduling.cpp
@@ -2,7 +2,39 @@
 // main file for hw 2
 
 #include <iostream>
+#include <fstream>
 #include <iomanip>
 #include <algorithm>
 #include <vector>
 #include "course.h"
+
+using namespace std;
+
+string convertDay(string dayDigit) {
+	return dayMap[dayDigit];
+}
+
+int main(int argc, char* argv[]) {
+
+	ifstream
+
+	if (argc==4) {
+		if (argv[3]=="room") {
+			// find rooms
+		} else if (argv[3]=="custom") {
+			// find the times when rooms are empty.
+		}
+	} else if (argc==5) {
+		if (argv[3]=="room") {
+			// write func that only saves courses that are in that room from argv[4]
+		} else if (argv[3]=="dept") {
+
+		}
+	} else {
+		cerr << "ERROR: Incorrect arguments inputted.\n";
+	}
+
+	vector<Course> courses;
+
+	// do stuff
+}
