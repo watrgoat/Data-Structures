f78989d641db97fe4dda0cb907fb64e6c847d7ec
watrgoat <oliveradams@iceeducate.org>
2023-09-20 18:08:10 -0400

    added some testing

---
+++ tests/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int *p = new int;
+	*p = 10;
+	int *q = &p;
+	cout << p << " " << q << endl;
+	p = new int;
+	*p = 1;
+	cout << *p << " " << *q << endl;
+}
