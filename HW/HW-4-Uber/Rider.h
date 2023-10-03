59d09ba80d8b9019efaafd12c934f37552ff1ca4
watrgoat <oliveradams@iceeducate.org>
2023-10-03 15:19:42 -0400

    worked on hw4

--- HW/HW-4-Uber/Rider.h
+++ HW/HW-4-Uber/Rider.h
@@ -3,85 +3,87 @@
 
 #include <string>
 
+using std::string;
+
 class Rider {
 private:
-    std::string firstName;
-    std::string lastName;
-    std::string gender;
+    string firstName;
+    string lastName;
+    string gender;
     int age;
-    std::string phoneNumber;
-    std::string rating;
-    std::string pickupLocationName;
+    string phoneNumber;
+    string rating;
+    string pickupLocationName;
     double pickupLatitude;
     double pickupLongitude;
-    std::string dropoffLocationName;
+    string dropoffLocationName;
     double dropoffLatitude;
     double dropoffLongitude;
-    std::string vehiclePreference;
-    std::string currentState;
-    std::string driverFirstName;
-    std::string driverLastName;
-    std::string driverPhoneNumber;
+    string vehiclePreference;
+    string currentState;
+    string driverFirstName;
+    string driverLastName;
+    string driverPhoneNumber;
 
 public:
     Rider();
     
     // Constructor
-    Rider(const std::string& firstName,
-          const std::string& lastName,
-          const std::string& gender,
+    Rider(const string& firstName,
+          const string& lastName,
+          const string& gender,
           int age,
-          const std::string& phoneNumber,
-          const std::string& pickupLocationName,
-          const std::string& rating,
+          const string& phoneNumber,
+          const string& pickupLocationName,
+          const string& rating,
           double pickupLatitude,
           double pickupLongitude,
-          const std::string& dropoffLocationName,
+          const string& dropoffLocationName,
           double dropoffLatitude,
           double dropoffLongitude,
-          const std::string& vehiclePreference,
-          const std::string& currentState,
-          const std::string& driverFirstName,
-          const std::string& driverLastName,
-          const std::string& driverPhoneNumber);
+          const string& vehiclePreference,
+          const string& currentState,
+          const string& driverFirstName,
+          const string& driverLastName,
+          const string& driverPhoneNumber);
 
     // Getter methods
-    std::string getFirstName() const;
-    std::string getLastName() const;
-    std::string getGender() const;
+    string getFirstName() const;
+    string getLastName() const;
+    string getGender() const;
     int getAge() const;
-    std::string getPhoneNumber() const;
-    std::string getRating() const;
-    std::string getPickupLocationName() const;
+    string getPhoneNumber() const;
+    string getRating() const;
+    string getPickupLocationName() const;
     double getPickupLatitude() const;
     double getPickupLongitude() const;
-    std::string getDropoffLocationName() const;
+    string getDropoffLocationName() const;
     double getDropoffLatitude() const;
     double getDropoffLongitude() const;
-    std::string getVehiclePreference() const;
-    std::string getCurrentState() const;
-    std::string getDriverFirstName() const;
-    std::string getDriverLastName() const;
-    std::string getDriverPhoneNumber() const;
+    string getVehiclePreference() const;
+    string getCurrentState() const;
+    string getDriverFirstName() const;
+    string getDriverLastName() const;
+    string getDriverPhoneNumber() const;
 
     // Setter methods
-    void setFirstName(const std::string& firstName);
-    void setLastName(const std::string& lastName);
-    void setGender(const std::string& gender);
+    void setFirstName(const string& firstName);
+    void setLastName(const string& lastName);
+    void setGender(const string& gender);
     void setAge(int age);
-    void setPhoneNumber(const std::string& phoneNumber);
-    void setRating(const std::string& rating);
-    void setPickupLocationName(const std::string& locationName);
+    void setPhoneNumber(const string& phoneNumber);
+    void setRating(const string& rating);
+    void setPickupLocationName(const string& locationName);
     void setPickupLatitude(double latitude);
     void setPickupLongitude(double longitude);
-    void setDropoffLocationName(const std::string& locationName);
+    void setDropoffLocationName(const string& locationName);
     void setDropoffLatitude(double latitude);
     void setDropoffLongitude(double longitude);
-    void setVehiclePreference(const std::string& preference);
-    void setCurrentState(const std::string& state);
-    void setDriverFirstName(const std::string& driverFirstName);
-    void setDriverLastName(const std::string& driverLastName);
-    void setDriverPhoneNumber(const std::string& driverPhoneNumber);
+    void setVehiclePreference(const string& preference);
+    void setCurrentState(const string& state);
+    void setDriverFirstName(const string& driverFirstName);
+    void setDriverLastName(const string& driverLastName);
+    void setDriverPhoneNumber(const string& driverPhoneNumber);
 };
 
 #endif
--- HW/HW-4-Uber/nyride.cpp
+++ HW/HW-4-Uber/nyride.cpp
@@ -1,6 +1,7 @@
 #include <list>
 #include <iostream>
 #include <fstream>
+#include <regex>
 #include "Rider.h"
 #include "Driver.h"
 
@@ -13,136 +14,133 @@ using namespace std;
 // efficiency?
 // how to use std::list in this?
 
-void readDriverFile(ifstream &file, list<Drivers> &inDrivers) {
+void readDriverFile(ifstream &file, list<Driver*>* &inDrivers) {
 	string word;
 
 	int i = 0;
 	// loop over driver text
 	// assign values to temp Driver obj then adds to list
 	while (file >> word) {
-		Driver curr;
+		Driver* curr = new Driver;
 		switch (i) {
 		case 0:
-			curr.setFirstName(word);
+			(*curr).setFirstName(word);
 			break;
 		case 1:
-			curr.setLastName(word);
+			(*curr).setLastName(word);
 			break;
 		case 2:
-			curr.setGender(word);
+			(*curr).setGender(word);
 			break;
 		case 3:
-			curr.setAge(stoi(word));
+			(*curr).setAge(stoi(word));
 			break;
 		case 4:
-			curr.setPhoneNumber(word);
+			(*curr).setPhoneNumber(word);
 			break;
 		case 5:
-			curr.setRating(word);
+			(*curr).setRating(word);
 			break;
 		case 6:
-			curr.setCurrentLatitude(stod(word));
+			(*curr).setCurrentLatitude(stod(word));
 			break;
 		case 7:
-			curr.setCurrentLongitude(stod(word));
+			(*curr).setCurrentLongitude(stod(word));
 			break;
 		case 8:
-			curr.setVehicleType(word);
+			(*curr).setVehicleType(word);
 			break;
 		case 9:
-			curr.setCurrentState(word);
+			(*curr).setCurrentState(word);
 			break;
 		case 10:
-			curr.setRiderFirstName(word);
+			(*curr).setRiderFirstName(word);
 			break;
 		case 11:
-			curr.setRiderLastName(word);
+			(*curr).setRiderLastName(word);
 			break;
 		case 12:
-			curr.setRiderPhoneNumber(word);
+			(*curr).setRiderPhoneNumber(word);
 			break;
 		}
 		i++;
 		if (i>12) {
 			i=0;
-			inDrivers.push_back(curr);
+			inDrivers->push_back(curr);
 		}
 	}
 }
 
-void readRiderFile(ifstream &file, list<Rider> &inRiders) {
+void readRiderFile(ifstream &file, list<Rider*>* &inRiders) {
 	string word;
 
 	int i = 0;
 	while (file >> word) {
-		Rider curr;
+		Rider* curr = new Rider;
 		switch (i) {
 		case 0:
-			curr.setFirstName(word);
+			(*curr).setFirstName(word);
 			break;
 		case 1:
-			curr.setLastName(word);
+			(*curr).setLastName(word);
 			break;
 		case 2:
-			curr.setGender(word);
+			(*curr).setGender(word);
 			break;
 		case 3:
-			curr.setAge(stoi(word));
+			(*curr).setAge(stoi(word));
 			break;
 		case 4:
-			curr.setPhoneNumber(word);
+			(*curr).setPhoneNumber(word);
 			break;
 		case 5:
-			curr.setRating(word);
+			(*curr).setRating(word);
 			break;
 		case 6:
-			curr.setPickupLocationName(word);
+			(*curr).setPickupLocationName(word);
 			break;
 		case 7:
-			curr.setPickupLatitude(stod(word));
+			(*curr).setPickupLatitude(stod(word));
 			break;
 		case 8:
-			curr.setPickupLongitude(stod(word));
+			(*curr).setPickupLongitude(stod(word));
 			break;
 		case 9:
-			curr.setDropoffLocationName(word);
+			(*curr).setDropoffLocationName(word);
 			break;
 		case 10:
-			curr.setDropoffLatitude(stod(word));
+			(*curr).setDropoffLatitude(stod(word));
 			break;
 		case 11:
-			curr.setDropoffLongitude(stod(word));
+			(*curr).setDropoffLongitude(stod(word));
 			break;
 		case 12:
-			curr.setVehiclePreference(word);
+			(*curr).setVehiclePreference(word);
 			break;
 		case 13:
-			curr.setCurrentState(word);
+			(*curr).setCurrentState(word);
 			break;
 		case 14:
-			curr.setDriverFirstName(word);
+			(*curr).setDriverFirstName(word);
 			break;
 		case 15:
-			curr.setDriverLastName(word);
+			(*curr).setDriverLastName(word);
 			break;
 		case 16:
-			curr.setDriverPhoneNumber(word);
+			(*curr).setDriverPhoneNumber(word);
 			break;
 		}
 		i++;
 		if (i>16) {
 			i=0;
-			inRiders.push_back(curr);
+			inRiders->push_back(curr);
 		}
 	}
 }
 
-bool checkPhoneNumber(const string &num) {
-	if (num.length()==10 && ) {
-
-	} else {
-		return false;
-	}
+bool isPhoneNumber(const string &num) {
+	regex pattern("^\\d{3}-\\d{3}-\\d{4}$");
+    return regex_match(num, pattern);
 }
 
 int main(int argc, char* argv[]) {
@@ -152,10 +150,26 @@ int main(int argc, char* argv[]) {
 		exit(1);
 	}
 
-	list<Rider> riders;
-	list<Driver> drivers;
+	// allocate lists onto heap containing riders and drivers also on the heap
+	list<Rider*> *riders = new list<Rider*>();
+	list<Driver*> *drivers = new list<Driver*>();
 
+	/*ifstream riderInFile(argv[1]);
+	ifstream driverInFile(argv[2]);
+	ofstream output1(argv[3]);
+	ofstream output2(argv[4]);
+	ofstream output3(argv[5]);*/
 
+	// Clean up: makes sure to delete all allocated memory from the list
+	
+	for (list<Rider*>::iterator it = riders->begin(); it != riders->end(); it++) {
+		delete *it;
+	}
+	delete riders;
+	for (list<Driver*>::iterator it = drivers->begin(); it != drivers->end(); it++) {
+		delete *it;
+	}
+	delete drivers;
 
 	return 0;
 }
