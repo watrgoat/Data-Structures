7def573b60c93d9526fffeb2043a62307ae591cf
watrgoat <oliveradams@iceeducate.org>
2023-11-08 17:52:51 -0500

    lets go lab11

--- labs/lab11/happy_number.cpp
+++ labs/lab11/happy_number.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <unordered_set>
 #include <vector>
-#include <string>
-
-
 
 using namespace std;
 
@@ -13,16 +10,103 @@ using namespace std;
 // how to determine if a number is going to infinitely loop?
 // if a number is going to infinitely loop, then it will eventually repeat a number
 #include <unordered_map>
-#include <vector>
-#include <string>
+
+class ListNode {
+public:
+    int value;
+    ListNode *next;
+    ListNode(int x) : value(x), next(nullptr) {}
+};
+
+class HashTable {
+private:
+    vector<ListNode*> table;  // table should store pointers to ListNode
+    int hashFunction(int key) const {
+        return key % table.size();
+    }
+
+public:
+    HashTable(int size) : table(size, nullptr) {}  // Initialize table with nullptrs
+
+    ~HashTable() {
+        for (ListNode* head : table) {
+            while (head != nullptr) {
+                ListNode* temp = head;
+                head = head->next;
+                delete temp;
+            }
+        }
+    }
+
+    bool insert(int key) {
+        // insert key into hash table
+        int index = hashFunction(key);
+        ListNode* &head = table[index];  // Reference to pointer
+
+        ListNode* node = head;
+        while (node != nullptr) {
+            if (node->value == key) {
+                return false;  // Key already exists
+            }
+            node = node->next;
+        }
+
+        ListNode* newNode = new ListNode(key);
+        newNode->next = head;
+        head = newNode;
+        return true;
+    }
+
+    ListNode* search(int key) const {
+        // search for key in hash table
+        int index = hashFunction(key);
+        ListNode* node = table[index];
+
+        while (node != nullptr) {
+            if (node->value == key) {
+                return node;
+            }
+            node = node->next;
+        }
+        return nullptr;
+    }
+};
+
+bool isHappy3(int n, HashTable& seen) {
+    if (n == 1) {
+        return true;
+    }
+    if (seen.search(n)) {
+        return false;
+    }
+    seen.insert(n);
+
+    int sum = 0;
+    int digit;
+
+    while (n > 0) {
+        digit = n % 10;
+        sum += digit * digit;
+        n /= 10;
+    }
+
+    return isHappy3(sum, seen);
+}
+
+bool isHappy3(int n) {
+    HashTable seen(100); // 100 is arbitrary, ideally should be based on expected range of input
+    return isHappy3(n, seen);
+}
 
 bool isHappy2(int n, std::unordered_map<int, bool> &seen) {
     if (n == 1) {
         return true;
     }
     if (seen.find(n) != seen.end()) {
-        return seen[n];
+        return false;
     }
+
+    seen[n] = false;
 
     std::vector<int> digits;
     while (n > 0) {
@@ -34,9 +118,8 @@ bool isHappy2(int n, std::unordered_map<int, bool> &seen) {
     for (int digit : digits) {
         sum += digit * digit;
     }
-
-    seen[n] = isHappy2(sum, seen);  // Store the result of the sum
-    return seen[n];
+    
+    return isHappy2(sum, seen);
 }
 
 bool isHappy2(int n) {
@@ -82,7 +165,7 @@ int main() {
     int testCases[] = {2,4,5,6,17,18,20,1,7,10,13,19,23,28,68};
 
     for (int n : testCases) {
-        if (isHappy(n)) {
+        if (isHappy3(n)) {
             std::cout << n << " is a happy number." << std::endl;
         } else {
             std::cout << n << " is not a happy number." << std::endl;
